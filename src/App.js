5a9bc7671d73ea3c085d77fb11d6cd2417f2006b
Zahra Rezaei <zahraarezaeii@outlook.com>
2024-08-12 18:37:54 +0330

    ReactRouterTemplate #5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Profile } from './pages/Profile';
+import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route></Route>
-        <Route></Route>
-      </Routes>
+        <div>Header</div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path='*' element={<div>Not Found Page.</div>} />
+        </Routes>
+        <div>Footer</div>
       </BrowserRouter>
     </div>
   );
